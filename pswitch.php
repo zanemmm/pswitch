d9c741383703257e698314a5fb0a182c92cdf2f3
pizane <pi@echo.cool>
2017-07-23 23:35:37 +0800

    upgrade

--- pswitch.php
+++ pswitch.php
@@ -3,28 +3,45 @@
 
 process($argv);
 
+/**
+ * process argv and start
+ *
+ * @param $argv
+ */
 function process($argv)
 {
-    if (in_array('--help', $argv) || (count($argv) <= 1)) {
-        displayHelp();
-        exit(0);
-    }
     $config = init();
     in_array('-a', $argv) && addSoftware($config);
     in_array('-s', $argv) && switchSoftware($argv, $config);
+    displayHelp();
 }
 
+/**
+ * init
+ *
+ * @param string $path
+ * @return array
+ */
 function init($path = '/etc/switch.json')
 {
     $emptyConfig = [];
     if (file_exists($path)) {
+        //decode config json to array
         $config = json_decode(file_get_contents($path), true);
     } else {
-        touch($path);
+        //make new config file
+        if (is_writeable(dirname($path))) {
+            touch($path);
+        } else {
+            error("can't create config file [$path]. check the permission to write");
+        }
     }
-    return isset($config) ? $config : $emptyConfig;
+    return empty($config) ? $emptyConfig : $config;
 }
 
+/**
+ * display help information
+ */
 function displayHelp()
 {
     echo <<<EOF
@@ -41,15 +58,21 @@ Options
 EOF;
 }
 
+/**
+ * add software info to config
+ *
+ * @param $config
+ */
 function addSoftware($config)
 {
+    //get the software info
     fwrite(STDOUT, 'Please input software name:');
     $name = trim(fgets(STDIN));
-    empty($name) && exit('software name is required' . PHP_EOL);
+    empty($name) && error('software name is required');
 
     fwrite(STDOUT, 'Please input software absolute path (bin directory or file):');
     $path = trim(fgets(STDIN));
-    empty($path) && exit('software path is required' . PHP_EOL);
+    empty($path) && error('software path is required');
     $path = rtrim($path, '/');
 
     fwrite(STDOUT, 'Please input software version (default: 1.0.0):');
@@ -61,11 +84,28 @@ function addSoftware($config)
     empty($linkDir) && ($linkDir = '/usr/local/bin');
     $linkDir = rtrim($linkDir, '/');
 
+
+    //get all file path from software path
+    $files = [];
+    if (is_dir($path)) {
+        $dir = dir($path);
+        while ($filename = $dir->read()) {
+            $file = $path . '/' . $filename;
+            if (!is_dir($file) && ($filename != '.') && ($filename != '..')) {
+                $files[] = $file;
+            }
+        }
+        $dir->close();
+    } else {
+        $files[] = $path;
+    }
+
+    //set all software info
     $info = [];
-    $info['version'] = $version;
-    $info['path'] = $path;
+    $info['files'] = $files;
     $info['linkDir'] = $linkDir;
 
+    //make sure the software version does not exist or replace it
     if (isset($config[$name][$version])) {
         fwrite(STDOUT, 'this software version is exist, replace it? (y/n, default:n):');
         $temp = trim(fgets(STDIN));
@@ -73,24 +113,34 @@ function addSoftware($config)
             exit(0);
         }
     }
-    $config[$name][$version] = $info;
 
+    //save info
+    $config[$name][$version] = $info;
     saveConfig($config);
 
     exit(0);
 }
 
+/**
+ * switch software version
+ *
+ * @param $argv
+ * @param $config
+ */
 function switchSoftware($argv, $config)
 {
+    //make sure $argv & $config are right
     if ((count($argv) != 4) || ($argv[1] != '-s')) {
-        exit('format error! e.g: pswitch -s [software name] [software version]');
+        error('wrong input! e.g: pswitch -s [software name] [software version]');
     }
     $name = $argv[2];
     $version = $argv[3];
     $info = [];
     if (empty($config)) {
-        exit('error! the config is empty, please add software!');
+        error('the config is empty, please add software!');
     }
+
+    //find the version of this software
     foreach ($config as $softwareName => $software) {
         if ($softwareName == $name) {
             foreach ($config[$softwareName] as $softwareVersion => $value) {
@@ -103,60 +153,57 @@ function switchSoftware($argv, $config)
         }
     }
     if (empty($info)) {
-        exit("error! can't find this software or version" . PHP_EOL);
+        error("can't find this software or version");
     }
-    if (is_dir($info['path'])) {
-        $dir = dir($info['path']);
-        while ($file = $dir->read()) {
-            $target = $info['path'] . '/' . $file;
-            if (!is_dir($target) && ($file != '.') && ($file != '..')) {
-                linkSoftware($target, $info['linkDir'], $config);
-            }
-        }
-        $dir->close();
-    } else {
-        if (file_exists($info['path'])) {
-            linkSoftware($info['path'], $info['linkDir'], $config);
+
+    //make sure symlink path are writable
+    if (!is_writeable($info['linkDir'])) {
+        error('the link path can\' write, check the permission');
+    }
+
+    //link all files of this software
+    foreach ($info['files'] as $file) {
+        if (file_exists($file)) {
+            linkSoftware($name, $file, $info['linkDir'], $config);
         } else {
-            exit("software not found, please check the software path: {$info['path']}" . PHP_EOL);
+            echo "\033[0;31m Warning: file [ $file ] not found, please check it." . PHP_EOL;
         }
     }
+
     exit(0);
 }
 
-function linkSoftware($target, $linkDir, $config)
+function linkSoftware($softwareName, $target, $linkDir, $config)
 {
+    //set the symlink path
     $temp = explode('/', $target);
     $link = $linkDir . '/' . end($temp);
 
-    if (checkSymlink($link, $config)) {
+    //set symlink
+    if (checkSymlink($softwareName, $link, $config)) {
         symlink($target, $link);
     } else {
-        fwrite(STDOUT, "can't create symlink because link: $link exist" . PHP_EOL);
+        echo "\033[0;31m Warning: can't create symlink because $link exist" . PHP_EOL;
     }
 }
 
-function checkSymlink($link, $config)
+function checkSymlink($softwareName, $link, $config)
 {
     if (file_exists($link)) {
-        $realPath = realpath($link);
-        if (is_link($realPath)) {
-            $realPath = readlink($realPath);
+        if (!is_link($link)) {
+            return false;
         }
-        $realPath = dirname($realPath);
+        $realPath = readlink($link);
+        //if this link in config, delete it
         $inConfig = false;
-        if (!empty($config)) {
-            foreach ($config as $software) {
-                if (!$inConfig) {
-                    foreach ($software as $version) {
-                        if ($realPath == $version['path']) {
-                            unlink($link);
-                            $inConfig = true;
-                            break;
-                        }
+        foreach ($config[$softwareName] as $version) {
+            if (!$inConfig) {
+                foreach ($version['files'] as $file) {
+                    if ($realPath == $file) {
+                        unlink($link);
+                        $inConfig = true;
+                        break;
                     }
-                } else {
-                    break;
                 }
             }
         }
@@ -167,6 +214,12 @@ function checkSymlink($link, $config)
 
 function saveConfig($config, $path = '/etc/switch.json')
 {
-    $config = json_encode($config);
+    $config = json_encode($config, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     file_put_contents($path, $config);
+}
+
+function error($info)
+{
+    echo "\e[0;31m Error: $info" . PHP_EOL;
+    exit(1);
 }
